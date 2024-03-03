cc2460ba4b048c7c5adfeab5df3d87fafbb0a5ee
MathiasSHAKOURI <mathias.shakouri@gmail.com>
2024-03-04 00:14:55 +0100

    added transition 1s on popup v2

--- resources/js/top-button.js
+++ resources/js/top-button.js
@@ -1,16 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 const goTopButton = document.getElementById("top-button");
+let timeoutId;
+goTopButton.style.opacity = "0";
 
 window.addEventListener("scroll", () => {
+  clearTimeout(timeoutId);
     if (window.scrollY > 10) {
+        goTopButton.style.transition = "opacity 0.5s";
         goTopButton.style.display = "block";
+
+        timeoutId = setTimeout(() => {
+          goTopButton.style.opacity = "1";
+        }, 500);
       } else {
-        goTopButton.style.display = "none";
+          goTopButton.style.transition = "opacity 0.5s";
+
+          timeoutId = setTimeout(() => {
+            goTopButton.style.opacity = "0";
+            setTimeout(() => {
+              goTopButton.style.display = "none";
+            }, 250);
+          }, 500);
       }
 });
 
 goTopButton.onclick = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0});
   };
 });
