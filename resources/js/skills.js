03707519c876e435443bdfea9f92c3d4edb40a14
MathiasSHAKOURI <mathias.shakouri@gmail.com>
2024-03-05 01:43:52 +0100

    modified click-banner link info

--- resources/js/skills.js
+++ resources/js/skills.js
@@ -125,7 +125,7 @@ const projects = [
       description: "Ce projet vise à explorer les concepts de reconnaissance du clavier en utilisant JavaScript.<br><br>" +
       "L'objectif est de créer une application légère permettant de détecter et de réagir aux entrées clavier en temps réel. En utilisant les fonctionnalités offertes par JavaScript, le projet offre une expérience interactive pour mieux comprendre le fonctionnement de la détection des touches du clavier dans un contexte web.<br><br>" +
       "Idéal pour les débutants cherchant à approfondir leurs connaissances en manipulation d'événements et en interaction avec l'interface utilisateur.<br><br>" +
-      "<a href='http://click-banner/'>Voir la démo ici</a>"
+      "<a href='/resources/click-banner/index.html'>Voir la démo ici</a>"
     },
     { 
         name: "Projets à venir",
